bc8ac10876f3f98f0d03f7bd640b050846de3217
Nadine Bautista <163831704+Nadine132@users.noreply.github.com>
2024-08-14 13:22:24 +0200

    terminado Home y realizado Zonas

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Imagen from '../Components/Imagen';
 import { Box, Typography } from '@mui/material';
 import CardGrid from '../Components/CardGrid';
+import Zonas from '../Components/Zonas';  // Importar el componente Zonas
 
 function Home() {
   return (
@@ -14,13 +15,23 @@ function Home() {
       {/* Contenedor de las recomendaciones y las tarjetas */}
       <Box sx={{ marginTop: 10, padding: 2 }}>
         {/* Texto "Recomendaciones" */}
-        <Typography variant="h4" component="h2" align="baseline" sx={{ marginBottom: 2}}>
+        <Typography variant="h4" component="h2" align="baseline" sx={{ marginBottom: 2 }}>
           Recomendaciones
         </Typography>
         
         {/* Contenedor para las tarjetas */}
         <CardGrid />  {/* Renderiza el grid de tarjetas */}
-        
+
+        {/* Espacio entre secciones */}
+        <Box sx={{ marginTop: 8 }}>
+          {/* Título "Zonas" */}
+          <Typography variant="h4" component="h2" align="baseline" sx={{ marginBottom: 2 }}>
+            Zonas
+          </Typography>
+
+          {/* Contenedor para las zonas */}
+          <Zonas />  {/* Renderiza las zonas */}
+        </Box>
       </Box>
     </div>
   );
